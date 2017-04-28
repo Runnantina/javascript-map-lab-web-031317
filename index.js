774882085645770e4cc7afc07932c03f2c99985d
Runnan Zhong <r.tina.zhong@gmail.com>
2017-04-28 13:03:22 -0400

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,22 @@ const robots = [
   { name: 'Ratchet', alliance: null }
 ];
 
+const sortedRobots = robots.map(function(robot){
+  if (knownDecepticons.includes(robot.name)) {
+    const decepticons = Object.assign({}, robot, {
+      alliance: 'decepticon'}
+    );
+    return decepticons;
+  }
+  else {
+    const autobots = Object.assign({}, robot,
+      {alliance: 'autobot'});
+    return autobots;
+  };
+})
+
+console.log(sortedRobots)
+
 const zebraStripes = [
   { width: 9.12, color: null },
   { width: 5.71, color: null },
@@ -21,3 +37,20 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null }
 ];
+
+// measuring against index
+// placing stripe color (either black or white) depending on index being odd or even
+
+// stripe index even = black
+// stripe index white = odd
+// callback we pass to .map() will take more than one parameter => .map(param1, param2)
+const coloredZebraStripes = zebraStripes.map(function(stripe, index){
+  if (index % 2 === 0) {
+    return Object.assign({}, stripe, {color: "black"});
+  }
+  else {
+    return Object.assign({}, stripe, {color: 'white'});
+  }
+});
+
+console.log(coloredZebraStripes);
